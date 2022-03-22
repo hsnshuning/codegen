e3e7f1c10890c35e426f07e89d5a800b5079c1fa
hsn <shuning_huang@qingsongchou.com>
2022-03-22 22:05:57 +0800

    修改一下模板

--- service/template.go
+++ service/template.go
@@ -1,47 +1,45 @@
 package service
 
 var typeMap = map[string]string{
-	"bigint": "int64",
-	"int":"int32",
-	"smallint":"int",
-	"tinyint":"int8",
-	"double":"float64",
-	"float":"float64",
-	"date":"time.Time",
-	"datetime":"time.Time",
-	"time":"time.Time",
-	"timestamp":"time.Time",
-	"bit":"int",
-	"bool":"bool",
-	"enum":"string",
-	"set":"string",
-	"char":"string",
-	"varchar":"string",
+	"bigint":    "int64",
+	"int":       "int64",
+	"smallint":  "int64",
+	"tinyint":   "int64",
+	"double":    "float64",
+	"float":     "float64",
+	"date":      "time.Time",
+	"datetime":  "time.Time",
+	"time":      "time.Time",
+	"timestamp": "time.Time",
+	"bit":       "int64",
+	"bool":      "bool",
+	"enum":      "string",
+	"set":       "string",
+	"char":      "string",
+	"varchar":   "string",
 }
 
 var modelTemplate = `
-package model
+========= START =========
+
 
 {{if .HasTime}}import "time"
 {{end}}
-type {{.StructName}} struct {`+"\n"+
-	"{{range .Fields}}	{{.Name}}\t{{.Type}}\t{{.Tag}}\t//{{.Comment}}\n{{end}}"+
-	"}\n"+
-`
+type {{.StructName}} struct {` + "\n" +
+	"{{range .Fields}}	{{.Name}}\t{{.Type}}\t{{.Tag}}\t//{{.Comment}}\n{{end}}" +
+	"}\n" +
+	`
 func ({{.StructName}}) TableName() string {
 	return "{{.TableName}}"
 }
+
+
+========= END =========
 `
 
-
 var DaoTemplate = `
-package dao
+========= START =========
 
-import (
-	"github.com/go-gorm/gorm"
-	"model"
-	"sync"
-)
 
 var _{{.StructName}} *{{.StructName}}
 
@@ -58,14 +56,14 @@ func New{{.StructName}} () (result *{{.StructName}}) {
 	return _{{.StructName}}
 }
 
-func (*{{.StructName}}) Insert (db gorm.DB, data *model.{{.StructName}}) (err error) {
+func (*{{.StructName}}) Insert (db *gorm.DB, data *model.{{.StructName}}) (err error) {
 	err = db.Create(data).Error
 	return 
 }
 
-func (*{{.StructName}}) GetOne (db gorm.DB, id int64) (result *model.{{.StructName}}, err error) {
+func (*{{.StructName}}) GetById (db *gorm.DB, id int64) (result *model.{{.StructName}}, err error) {
 	tmp := new(model.{{.StructName}})
-	err = db.Where(where).First(tmp).Error
+	err = db.Where("id = ?", id).First(tmp).Error
 	if err != nil {
 		if err == gorm.ErrRecordNotFound {
 			err = nil
@@ -73,11 +71,10 @@ func (*{{.StructName}}) GetOne (db gorm.DB, id int64) (result *model.{{.StructNa
 		return
 	}
 	result = tmp
-	err = db.First(result, id).Error
 	return 
 }
 
-func (*{{.StructName}}) GetList (db gorm.DB, where *model.{{.StructName}}, limit int64, offset int64) (result []*model.{{.StructName}}, err error) {
+func (*{{.StructName}}) Find (db *gorm.DB, where *model.{{.StructName}}, limit int64, offset int64) (result []*model.{{.StructName}}, err error) {
 	if limit <= 0 {
 		limit = 100
 	}
@@ -85,10 +82,32 @@ func (*{{.StructName}}) GetList (db gorm.DB, where *model.{{.StructName}}, limit
 	return
 }
 
-func (*{{.StructName}}) Update (db gorm.DB, where *model.{{.StructName}}, update *model.{{.StructName}}) (err error) {
+func (*{{.StructName}}) Update (db *gorm.DB, where *model.{{.StructName}}, update *model.{{.StructName}}) (err error) {
 	if where == nil || update == nil {
 		return errors.New("where or update is nil")
 	}
 	return db.Table(where.TableName()).Where(where).Updates(update).Error
 }
+
+func (dao *{{.StructName}}) FindAll(db *gorm.DB, where *model.{{.StructName}}) (result []*model.{{.StructName}}, err error) {
+	var (
+		limit  int64 = 100
+		offset int64 = 0
+	)
+	for {
+		list, e := dao.Find(db, where, limit, offset)
+		if e != nil {
+			err = errors.Wrap(e, "dao.GetList error")
+			return
+		}
+		result = append(result, list...)
+		if int64(len(list)) < limit {
+			break
+		}
+		offset += limit
+	}
+	return
+}
+
+========= END =========
 `
